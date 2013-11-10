8cd61c02d7addfc12fdd5309f5d4776255cbf160
Stéphane P. Péricat <contact@pericat.net>
2013-11-10 12:18:18 +0100

    NEW unit test starter

---
+++ test/stereoscope-test.js
@@ -0,0 +1,8 @@
+describe('stereoscope.js', function() {
+    var S = window.Stereoscope;
+
+    it('is an object', function(done) {
+        expect(S).to.be.an('object');
+        done();
+    });
+});
